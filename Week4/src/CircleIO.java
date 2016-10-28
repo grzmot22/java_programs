3796ae2a347442e6e96eebdee2291690060bd3e8
grzmot22 <grzmot22@users.noreply.github.com>
2016-10-28 10:24:13 +0100

    Add more comments

--- Week4/src/CircleIO.java
+++ Week4/src/CircleIO.java
@@ -9,23 +9,29 @@ import java.util.Scanner;
  * @version 24.10.16
  */
 
-public class CircleIO {			
-final static double PI = 3.142857;		
-public CircleIO()		
-{		
-Scanner keyboard = new Scanner(System.in);		
-double radius = keyboard.nextDouble();		
-double circumference = 2 * PI * radius;		
-double area = PI * radius * radius; //(the square of the radius, or radius * radius)		
-double volume = 4 * PI * radius * radius * radius / 3;		
-String output1 = "Circumference of a circe is ";		
-String output2 = "Area of a circe is ";		
-String output3 = "Volume of a circe is ";		
-System.out.println(output1 + circumference);		
-System.out.println(output2 + area);		
-System.out.println(output3 + volume);		
-System.out.printf(output1 + circumference +"\n");		
-System.out.printf(output2 + area +"\n");		
-System.out.printf(output3 + volume +"\n");	
-}
+public class CircleIO {
+       
+               final static double PI = 3.142857;
+               public CircleIO()
+               {
+               Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a radius of circle: ");
+               double radius = keyboard.nextDouble();//collect number of radius from user
+               double circumference = 2 * PI * radius; //Count circumference of a circle
+               double area = PI * radius * radius; //Count area of a sphere
+               double volume = 4 * PI * radius * radius * radius / 3; //Count volume of a sphere
+		//Define strings
+               String output1 = "Circumference of a circe is ";
+               String output2 = "Area of a circe is ";
+               String output3 = "Volume of a circe is ";
+		//Print results using System.out.println()
+               System.out.println(output1 + circumference);
+               System.out.println(output2 + area);
+               System.out.println(output3 + volume);
+		//Print results using System.out.printf()
+               System.out.printf(output1 + circumference +"\n");
+               System.out.printf(output2 + area +"\n");
+               System.out.printf(output3 + volume +"\n");
+
+       }
 }
