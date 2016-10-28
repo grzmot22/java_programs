8e7cce003b8f29ceebc17944c3b4a08dc15e2182
grzmot22 <grzmot22@users.noreply.github.com>
2016-10-28 01:15:29 +0100

    Add more

--- Week4/src/MarkAverageIO.java
+++ Week4/src/MarkAverageIO.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
  * @author Boguslaw Smolarczyk 1612761 
  * @version 24.10.16
  */
-
+ 
 public class MarkAverageIO {
-	public MarkAverageIO()
-	{
-		Scanner keyboard = new Scanner(System.in);
-		
-		double mark1, mark2, mark3, average;
-		
-		System.out.println("Type first mark: " );
-		mark1 = keyboard.nextDouble();
-		System.out.println("Type second mark: " );
-		mark2 = keyboard.nextDouble();
-		System.out.println("Type third mark: " );
-		mark3 = keyboard.nextDouble();
-		average = (mark1 + mark2 + mark3)/3;
-		System.out.println("The average from " + mark1 + " "  + mark2 + " " + mark3 + " is equal to " + average );
-	}
+       public MarkAverageIO()
+       {
+               Scanner keyboard = new Scanner(System.in);
+               
+               double mark1, mark2, mark3, average; //Define variables
+               //collect marks from user
+               System.out.println("Type first mark: " );
+               mark1 = keyboard.nextDouble(); 
+               System.out.println("Type second mark: " );
+               mark2 = keyboard.nextDouble(); 
+               System.out.println("Type third mark: " );
+               mark3 = keyboard.nextDouble(); 
+               average = (mark1 + mark2 + mark3)/3; // Count average from 3 marks
+               System.out.println("The average from " + mark1 + " "  + mark2 + " " + mark3 + " is equal to " + average );
+       }
 
 }
+
