15eda424317e6789dd9b534ac6f5fd2daec6d14c
grzmot22 <grzmot22@users.noreply.github.com>
2016-12-02 12:01:04 +0000

    Add menu and fix loop

--- GreenBottles.java
+++ GreenBottles.java
@@ -1,4 +1,6 @@
-**
+import java.util.Scanner;
+
+/**
  * Created by grzmot22
  */
 public class GreenBottles {
@@ -32,8 +34,6 @@ public class GreenBottles {
             System.out.println(bottlesLoop + " green bottles standing on the wall,");
             System.out.println("And if 1 green bottle should accidently fall,");
             System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
-
-           // System.out.println(i); check
             continue;
         }
         String lessBottle = "no";
@@ -45,27 +45,51 @@ public class GreenBottles {
     public  void xyGreenBottles(int x, int y) {
         bottles = x;
         failBottle =y;
-        for (int i = -(x-(y*y));i <= bottles; i++) {
-            bottlesLoop = bottles-=failBottle;
+        for (int i = -(x/2)+((y*y));i <= bottles; i++) {
+            bottlesLoop = bottles;
             lessBottle = bottles - failBottle;
             System.out.println(bottlesLoop + " green bottles standing on the wall,");
             System.out.println(bottlesLoop + " green bottles standing on the wall,");
             System.out.println("And if " +failBottle + " green bottle should accidently fall,");
             System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
-
-             System.out.println(i); //ckeck
+            bottlesLoop = bottles-=failBottle;
             continue;
         }
         String lessBottle = "no";
         System.out.println(bottles + " green bottles standing on the wall,");
         System.out.println(bottles + " green bottles standing on the wall,");
-        System.out.println("And if " +lessBottle + " green bottle should accidently fall,");
+        System.out.println("And if " +failBottle + " green bottle should accidently fall,");
         System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
     }
         public static void main(String[] args){
             GreenBottles myBottles = new GreenBottles();
-         //   myBottles.tenGreenBottles();
-         //   myBottles.xGreenBottles(40);
-            myBottles.xyGreenBottles(40,2);
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("What you like to run:");
+            System.out.println("Enter 1 to run 10 Green Bottles.");
+            System.out.println("Enter 2 to run 40 Green bottles.");
+            System.out.println("Enter 3 to run 50 Green Bottles with 5 fall.");
+            System.out.println("Enter 4 to exit.");
+               int choose = keyboard.nextInt();
+               switch (choose){
+                   case 1: {
+                       myBottles.tenGreenBottles();
+                   }
+                   break;
+                   case 2: {
+                       myBottles.xGreenBottles(40);
+                   }
+                   break;
+                   case 3: {
+                       myBottles.xyGreenBottles(50,5);
+                   }
+                   break;
+                   case 4: {
+                       System.out.print("Bye!");
+                   }
+                   break;
+                   default: {
+                       System.out.println("You entered wrong number!");
+                   }
+               }
     }
 }
