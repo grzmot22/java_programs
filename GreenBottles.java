bba1bd869ab898e27a790e3c57d32c1b141e94ee
grzmot22 <grzmot22@users.noreply.github.com>
2016-12-07 17:34:48 +0000

    Update GreenBottles.java

--- GreenBottles.java
+++ GreenBottles.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-/**
- * Created by grzmot22
- */
-public class GreenBottles {
-    private int bottles;
-    private  int lessBottle;
-    private  int failBottle;
-    private  int bottlesLoop;
-    public void tenGreenBottles() {
+public class TenGreenBottles {
+    private static int bottles;
+    private static int lessBottle;
+    private static int failBottle;
+    private static int bottlesLoop;
+    
+    public static void tenGreenBottles() {
         bottles = 10;
         for (int i = -6; i <= bottles; i++) {
             lessBottle = bottles - 1;
@@ -25,7 +23,7 @@ public class GreenBottles {
         System.out.println("And if 1 green bottle should accidently fall,");
         System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
     }
-    public  void xGreenBottles(int x) {
+    public static void xGreenBottles(int x) {
             bottles = x;
         for (int i = 0-(x-3); i <= bottles; i++) {
             bottlesLoop = bottles--;
@@ -42,7 +40,7 @@ public class GreenBottles {
         System.out.println("And if 1 green bottle should accidently fall,");
         System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
     }
-    public  void xyGreenBottles(int x, int y) {
+    public static void xyGreenBottles(int x, int y) {
         bottles = x;
         failBottle =y;
         for (int i = -(x/2)+((y*y));i <= bottles; i++) {
@@ -61,8 +59,7 @@ public class GreenBottles {
         System.out.println("And if " +failBottle + " green bottle should accidently fall,");
         System.out.println("There’ll be " + lessBottle + " green bottles standing on the wall.");
     }
-        public static void main(String[] args){
-            GreenBottles myBottles = new GreenBottles();
+       public static void main(String[] args){
             Scanner keyboard = new Scanner(System.in);
             System.out.println("What you like to run:");
             System.out.println("Enter 1 to run 10 Green Bottles.");
@@ -72,15 +69,15 @@ public class GreenBottles {
                int choose = keyboard.nextInt();
                switch (choose){
                    case 1: {
-                       myBottles.tenGreenBottles();
+                       tenGreenBottles();
                    }
                    break;
                    case 2: {
-                       myBottles.xGreenBottles(40);
+                       xGreenBottles(40);
                    }
                    break;
                    case 3: {
-                       myBottles.xyGreenBottles(50,5);
+                       xyGreenBottles(50,5);
                    }
                    break;
                    case 4: {
